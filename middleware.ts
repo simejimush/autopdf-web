bda32d47d32961a7f5b64ef08ec7191c0ca2f2b1
mushkazusira <xxbbdyu@gmail.com>
2026-02-20 21:46:49 +0900

    update cron debug

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/dashboard/:path*", "/rules/:path*", "/me/:path*"],
 };
